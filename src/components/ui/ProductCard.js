8b3494b631d32ac0f29ba265e31553ac966ff4cf
nicetryharshit <harshitdubeyhd@gmail.com>
2023-05-18 01:44:56 +0530

    display book title and author

--- src/components/ui/ProductCard.js
+++ src/components/ui/ProductCard.js
@@ -3,12 +3,12 @@ import '../../styles/product_card.css';
 
 export default function ProductCard({ props })
 {
-    const { author } = props;
-    console.log(props);
+    const { author, title } = props;
     return (
         <div className="product-card">
             {/* <img src={imagePath} alt={`Image for the book: ${title}`} /> */}
-            {/* <h2>{title}</h2> */}
+            <h2>{title}</h2>
+            <h3>{author}</h3>
             <button>ADD TO CART</button>
         </div>
     );
